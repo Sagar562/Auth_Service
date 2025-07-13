935fda808c7d524e06b3940b5f71f13c1cc236a9
sagar <vyassagar4321@gmail.com>
2025-07-13 13:19:19 +0530

    First commit with basic setup for AUTH-SERVICE

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const { PORT } = require('./config/serverConfig');
+
+const startServer = () => {
+
+    app.use(bodyParser.json()); 
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.listen(PORT, () => {
+        console.log(`server started on Port: ${PORT}`);
+    })
+
+}
+
+startServer();
